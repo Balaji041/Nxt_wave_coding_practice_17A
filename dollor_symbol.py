088d024afa14b2ce811c34fa6f68027ebc672bfb
balaji <76476394+Balaji041@users.noreply.github.com>
2023-08-29 19:57:06 +0530

    Create dollor_symbol.py

---
+++ dollor_symbol.py
@@ -0,0 +1,15 @@
+n = int(input())
+for i in range(1, n+1):
+    row_out = " " * (n-i)
+    row_out = row_out + "$" * n
+    print(row_out)
+
+"""
+input:5
+output:
+    $$$$$
+   $$$$$
+  $$$$$
+ $$$$$
+$$$$$
+"""
